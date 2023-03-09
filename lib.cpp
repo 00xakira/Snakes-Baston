58d3985be2e2afadc3d268bf0b02268ee87ef99b
00xakira <eweeda12@gmail.com>
2023-03-09 19:33:58 +0100

    checker

---
+++ lib.cpp
@@ -0,0 +1,104 @@
+enum whereIssnake{
+    upperleft,
+    upperright,
+    lowerleft,
+    lowerright,
+    uppermiddle,
+    lowermiddle,
+    middleright,
+    middleleft,
+    middle,
+}; 
+
+/*
+    int HEIGHT;
+    int WIDTH;
+*/
+
+// done sccusefully 
+int AreThereObstaclesArroundsnake(int row, int col, int board[][WIDTH]){
+    enum whereIssnake where;
+    if (row==0 && col==0){
+        where = upperleft;
+    }
+    else if (row==0 && col==15){
+        where = upperright;
+    }
+    else if (row==15 && col==0){
+        where = lowerleft;
+    }
+    else if (row==15 && col==15){
+        where = lowerright;
+    }
+    else if (row>0 && row<15 && col==0){
+        where = middleleft;
+    }
+    else if (row>0 && row<15 && col==15){
+        where = middleright;
+    }
+    else if (row==0 && col>0 && col<15){
+        where = uppermiddle;
+    }
+    else if (row==15 && col>0 && col<15){
+        where = lowermiddle;
+    }
+    else if (row>0 && row<15 && col>0 && col<15){
+        where = middle;
+    }
+    else{
+        return 0;
+    }
+    switch(where){
+        case upperleft:
+            if (board[0][1]%10>0 && board[1][0]%10>0 && board[1][1]%10>0 ) {
+                return 1;
+            }
+            return 0;
+        case upperright:
+            if (board[0][8]%10>0 && board[1][9]%10>0 && board[1][8]%10>0 ) {
+                return 1;
+            }
+            return 0;
+        case lowerleft:
+            if (board[8][0]%10>0 && board[9][1]%10>0 && board[8][1]%10>0 ) {
+                return 1;
+            }
+            return 0;
+        case lowerright:
+            if (board[8][8]%10>0 && board[8][9]%10>0 && board[9][8]%10>0 ) {
+                return 1;
+            }
+            return 0;
+        case uppermiddle:
+            if (board[row+1][col]%10>0 && board[row+1][col+1]%10>0 && board[row][col+1]%10>0
+                && board[row][col-1]%10>0 && board[row+1][col-1]%10>0) {
+                return 1;
+            }
+            return 0;
+        case lowermiddle:
+            if (board[row][col+1]%10>0 && board[row-1][col+1]%10>0 && board[row-1][col]%10>0
+                && board[row-1][col-1]%10>0 && board[row][col-1]%10>0 ) {
+                return 1;
+            }
+            return 0;
+        case middleleft:
+            if (board[row+1][col]%10>0 && board[row+1][col+1]%10>0 && board[row][col+1]%10>0
+                && board[row-1][col+1]%10>0 && board[row-1][col]%10>0) {
+                return 1;
+            }
+            return 0;
+        case middleright:
+            if (board[row+1][col]%10>0 && board[row+1][col-1]%10>0 && board[row][col-1]%10>0
+                && board[row-1][col-1]%10>0 && board[row-1][col]%10>0) {
+                return 1;
+            }
+            return 0;
+        case middle:
+            if (board[row+1][col]%10>0 && board[row+1][col-1]%10>0 && board[row][col-1]%10>0
+                && board[row-1][col-1]%10>0 && board[row-1][col]%10>0 && board[row-1][col+1]%10>0
+                && board[row][col+1]%10>0 && board[row+1][col+1]%10>0) {
+                return 1;
+            }
+            return 0;
+    }
+}
